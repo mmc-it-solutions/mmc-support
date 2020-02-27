fa32eff231286e5d3b1cb14d4a947fc9a4e2b895
JRJman <41746794+JRJman@users.noreply.github.com>
2020-02-27 14:23:09 +0100

    add ticket popup screen is klaar

--- frontend/src/containers/01_home/Home.js
+++ frontend/src/containers/01_home/Home.js
@@ -3,26 +3,10 @@ import React from "react";
 import AddTicketPopup from "../../components/addTicketPopup/AddTicketPopup";
 
 class HomeScreen extends React.Component {
-  state = {
-    display: "none"
-  };
-
-  changeDisplay = () => {
-    const { display } = this.state;
-
-    this.setState({
-      display: display === "none" ? "flex" : "none"
-    });
-  };
-
   render() {
-    const { display } = this.state;
-
     return (
       <div>
         <h1>Home</h1>
-        <button onClick={this.changeDisplay}>Popup</button>
-        <AddTicketPopup display={display} />
       </div>
     );
   }
