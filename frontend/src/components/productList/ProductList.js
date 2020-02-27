b09f92f23029aa11631f0f551df1bb1b1d85a45c
JRJman <41746794+JRJman@users.noreply.github.com>
2020-02-27 15:52:01 +0100

    Product list aangepast naar een componant

--- frontend/src/components/productList/ProductList.js
+++ frontend/src/components/productList/ProductList.js
@@ -1,52 +1,51 @@
 import React from "react";
-import axios from "axios";
 
 import "./ProductList.css";
 
-class ProductList extends React.Component {
-  state = {
-    //dit is test data dit wordt later aangepast
-    products: [
-      {
-        id: 1,
-        name: "hello"
-      },
-      {
-        id: 2,
-        name: "admin"
-      },
-      {
-        id: 3,
-        name: "world"
-      },
-      {
-        id: 4,
-        name: "Joep"
-      }
-    ]
-  };
+const ProductList = ({ products }) => {
+  // dit is de code die je nodig hebt in de class om deze component te laten werken
 
-  render() {
-    const { products } = this.state;
+  // state = {
+  //   //dit is test data dit wordt later aangepast
+  //   products: [
+  //     {
+  //       id: 1,
+  //       name: "hello"
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "admin"
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "world"
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "Joep"
+  //     }
+  //   ]
+  // };
 
-    return (
-      <div className="productsList">
-        <h2 className="productsList__title">Product list</h2>
-        <table className="productsList__table">
-          <tr className="productsList__table--rowHead">
-            <th className="productsList__table--itemHead">Id</th>
-            <th className="productsList__table--itemHead">Name</th>
+  // <ProductList products={products} />
+
+  return (
+    <div className="productsList">
+      <h2 className="productsList__title">Product list</h2>
+      <table className="productsList__table">
+        <tr className="productsList__table--rowHead">
+          <th className="productsList__table--itemHead">Id</th>
+          <th className="productsList__table--itemHead">Name</th>
+        </tr>
+        {products.map(product => (
+          <tr className="productsList__table--row">
+            <td className="productsList__table--item">{product.id}</td>
+            <td className="productsList__table--item">{product.name}</td>
           </tr>
-          {products.map(product => (
-            <tr className="productsList__table--row">
-              <td className="productsList__table--item">{product.id}</td>
-              <td className="productsList__table--item">{product.name}</td>
-            </tr>
-          ))}
-        </table>
-      </div>
-    );
-  }
-}
+        ))}
+      </table>
+    </div>
+  );
+};
 
 export default ProductList;
