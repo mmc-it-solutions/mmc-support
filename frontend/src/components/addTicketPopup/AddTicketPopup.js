97c10c6e5d3d130404c610d5228315cd23a12e9e
JRJman <41746794+JRJman@users.noreply.github.com>
2020-02-27 12:09:50 +0100

    Extra changes

--- frontend/src/components/addTicketPopup/AddTicketPopup.js
+++ frontend/src/components/addTicketPopup/AddTicketPopup.js
@@ -3,10 +3,32 @@ import React from "react";
 import "./AddTicketPopup.css";
 
 class AddTicketPopup extends React.Component {
+  //  Om dit programma te laten werken moet deze code in de
+  //  class gaan staan waar deze component wordt toegevoegd
+
+  // state = {
+  //   display: "none"
+  // };
+
+  // changeDisplay = () => {
+  //   const { display } = this.state;
+  //   this.setState({
+  //     display: display === "none" ? "flex" : "none"
+  //   });
+  // };
+
+  // <AddTicketPopup display={display} />
+
   render() {
     return (
       <div>
-        <div>Popup</div>
+        <div
+          style={{
+            display: this.props.display
+          }}
+        >
+          Popup
+        </div>
       </div>
     );
   }
--- frontend/src/containers/01_home/Home.js
+++ frontend/src/containers/01_home/Home.js
@@ -1,15 +1,28 @@
 import React from "react";
 
-import { AddTicketpopup } from "../../components/addTicketPopup/AddTicketPopup";
+import AddTicketPopup from "../../components/addTicketPopup/AddTicketPopup";
 
 class HomeScreen extends React.Component {
-  changeDisplay = () => {};
+  state = {
+    display: "none"
+  };
+
+  changeDisplay = () => {
+    const { display } = this.state;
+
+    this.setState({
+      display: display === "none" ? "flex" : "none"
+    });
+  };
+
   render() {
+    const { display } = this.state;
+
     return (
       <div>
         <h1>Home</h1>
         <button onClick={this.changeDisplay}>Popup</button>
-        <addTicketPopup />
+        <AddTicketPopup display={display} />
       </div>
     );
   }
