4e6ac41d9ea35ef1e17eff287b68434764a62bcf
JRJman <41746794+JRJman@users.noreply.github.com>
2020-02-27 10:22:10 +0100

    Added product list

--- frontend/src/containers/01_home/Home.js
+++ frontend/src/containers/01_home/Home.js
@@ -1,12 +1,9 @@
 import React from "react";
 
-import ProductList from "../../components/productList/ProductList";
-
 const HomeScreen = () => {
   return (
     <div>
       <h1>Home</h1>
-      <ProductList />
     </div>
   );
 };
