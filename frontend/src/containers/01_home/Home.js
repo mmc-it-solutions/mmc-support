8ede4ef7ca38821d6699fc8d87377af0b22fcf5b
JRJman <41746794+JRJman@users.noreply.github.com>
2020-03-18 10:40:19 +0100

    change home back to normal

--- frontend/src/containers/01_home/Home.js
+++ frontend/src/containers/01_home/Home.js
@@ -1,48 +1,13 @@
 import React from "react";
 
-import { connect } from "react-redux";
-import { getTickets, updateTicketStatus } from "../../store/actions/ticket";
-
 class HomeScreen extends React.Component {
-  componentDidMount() {
-    this.props.getTickets();
-  }
-
-  changeState = (id, event) => {
-    let data = {
-      id: id,
-      newStatus: event.target.value
-    };
-
-    this.props.updateTicketStatus(data);
-  };
-
   render() {
     return (
       <div>
         <h1>Home</h1>
-        {this.props.tickets.map(ticket => (
-          <p>
-            {ticket.id} {ticket.name}:
-            <select
-              value={ticket.status}
-              onChange={this.changeState.bind(this, ticket.id)}
-            >
-              <option value="1">To do</option>
-              <option value="2">Doing</option>
-              <option value="3">Done</option>
-            </select>
-          </p>
-        ))}
       </div>
     );
   }
 }
 
-const mapStateProps = (state, ownProps) => ({
-  tickets: state.ticket.tickets
-});
-
-const mapDispatchToProps = { getTickets, updateTicketStatus };
-
-export default connect(mapStateProps, mapDispatchToProps)(HomeScreen);
+export default HomeScreen;
